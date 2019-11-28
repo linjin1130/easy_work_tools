3ec32499e36937ce79e6d4d19df697bedf06995d
linjin1130 <jin.lin@quantum-info.com>
2019-11-28 16:38:26 +0800

    并发模式DAC测试

---
+++ python3/da_control/da_control_batch_cmd.py
@@ -0,0 +1,131 @@
+from da_board import *
+from data_waves import *
+import filecmp
+
+import matplotlib.pyplot as plt
+
+new_ip = '10.0.5.149'
+da = DABoard(id='F149', ip=new_ip, data_offset=[0,0,0,0])
+board_status = da.connect()
+print(board_status)
+# da.write_command(0xdeadbeef, 0, 0, donot_ret=True)
+da.init_device()
+# da.Run_Command(26,0,0)
+# da.Init()
+# da.InitBoard()
+# da.SetIsMaster(1)
+# da.ClearTrigCount()
+# time.sleep(1)
+# da.SetLoop(1,2,3,4)
+# da.SetGain(3, 123)
+# rd_data = da.Read_RAM(0,1024)
+# print(rd_data)
+# wave = range(0,65536)#[1,2,3,1111,2222,2222]
+# print(len(wave))
+
+# da.Write_RAM(0,wave)
+# da.StartStop(240)
+# da.SetDefaultVolt(1,0)
+# da.SetDefaultVolt(2,15000)
+# da.SetDefaultVolt(3,30000)
+# da.SetDefaultVolt(4,60000)
+da_ctrl = waveform()
+# da_ctrl.generate_seq()
+da_ctrl.generate_sin(repeat=2048)
+da_ctrl.generate_seq()
+# da_ctrl.generate_trig_seq(loopcnt=1024)
+# print(len(da_ctrl.seq))
+# print(len(da_ctrl.wave))
+# print("wave")
+
+
+# plt.figure()
+# plt.plot(da_ctrl.wave)
+# plt.show()
+print(len(da_ctrl.wave))
+cnt=0
+# for i in range(1000000):
+#     da.set_para(0x01, 0x234567, 0x89abcdef)
+#     da.commit_para()
+#     time.sleep(2)
+
+print(len(da_ctrl.seq))
+# _start = time.time()
+# cycle_cnt = 1000
+# for i in range(cycle_cnt):
+#     da.write_seq(1,seq=da_ctrl.seq)
+#     da.write_wave(1,wave=da_ctrl.wave)
+#     da.write_seq(2,seq=da_ctrl.seq)
+#     da.write_wave(2,wave=da_ctrl.wave)
+#     da.write_seq(3,seq=da_ctrl.seq)
+#     da.write_wave(3,wave=da_ctrl.wave)
+#     da.write_seq(4,seq=da_ctrl.seq)
+#     da.write_wave(4,wave=da_ctrl.wave)
+#     cnt+=1
+#     print(cnt)
+#     #
+#     # # da.stop_output_wave(0)
+#     # da.set_trig_count_l1(10)
+#     # da.start_output_wave(0)
+#     # da.commit_para()
+#     # da.wait_response()
+#     # da.send_int_trig()
+#     # da.wait_response()
+#     # time.sleep(2)
+# _stop = time.time()
+#
+# print(f'cycle count: {cycle_cnt}, 4 channel wave data and seq write is: {_stop-_start}')
+
+da.set_monitor(1, '10.0.0.222')
+
+# da_ctrl.wave *= 2
+# da_ctrl.wave = da_ctrl.wave[0:20000]
+# da_ctrl.wave = [i for i in range(1024)]
+# da_ctrl.seq = da_ctrl.seq[:32]
+print(len(da_ctrl.wave))
+# print(da_ctrl.wave)
+# print(da_ctrl.seq)
+
+_start = time.time()
+cycle_cnt = 1
+for i in range(cycle_cnt):
+    _s = time.time()
+    da.write_seq(1,seq=da_ctrl.seq, fast=True)
+    da.write_wave(1,wave=da_ctrl.wave, fast=True)
+    da.write_seq(2,seq=da_ctrl.seq, fast=True)
+    da.write_wave(2,wave=da_ctrl.wave, fast=True)
+    da.write_seq(3,seq=da_ctrl.seq, fast=True)
+    da.write_wave(3,wave=da_ctrl.wave, fast=True)
+    # # # da.write_seq(4,seq=da_ctrl.seq[0:128], fast=True)
+    da.write_seq(4,seq=da_ctrl.seq, fast=True)
+    da.write_wave(4,wave=da_ctrl.wave, fast=True)
+    da.commit_mem()
+    da.wait_response()
+    cnt+=1
+    # print(cnt)
+    #
+    # da.stop_output_wave(0)
+    da.set_trig_count_l1(60000)
+    da.start_output_wave(1)
+    da.start_output_wave(2)
+    da.start_output_wave(3)
+    da.start_output_wave(4)
+    da.commit_para()
+    da.wait_response()
+    da.send_int_trig()
+    da.wait_response()
+    _e = time.time()
+    print(f'cnt:{cnt}, packet time is:{_e-_s}')
+    # time.sleep(2)
+_stop = time.time()
+
+print(f'cycle count: {cycle_cnt}, fast 4 channel wave data and seq write is: {_stop-_start}')
+
+# print(da.para_addr_list, da.para_data_list)
+# da.stop_output_wave(0)
+# print(da.para_addr_list, da.para_data_list)
+# da.commit_para()
+# da.wait_response()
+da.disconnect()
+if board_status < 0:
+    print('Failed to find board')
---
+++ python3/da_control/da_control_batch_cmd_multiboards.py
@@ -0,0 +1,134 @@
+import time
+import numpy as np
+from da_board import *
+from data_waves import *
+import filecmp
+
+import matplotlib.pyplot as plt
+# da_list = [113, 217, 188, 226, 149, 223, 153]
+# da_list = [164,]#, 217, 188, 226, 149, 223, 153]
+# da_list = [i for i in range(80)]
+da_list = [157, 217, 188, 149, 211, 223, 153, 226, 164, 154]
+das = [DABoard(id=f'F{id}', ip=f'10.0.5.{id}', data_offset=[0,0,0,0], batch_mode=True) for id in da_list]
+for da in das:
+    da.connect()
+    # da.init_tcp()
+    da.init_device()
+
+
+da_ctrl = waveform()
+# da_ctrl.generate_seq()
+da_ctrl.generate_sin(repeat=2048)
+da_ctrl.generate_seq()
+# da_ctrl.generate_trig_seq(loopcnt=1024)
+# print(len(da_ctrl.seq))
+# print(len(da_ctrl.wave))
+# print("wave")
+da_ctrl.wave = da_ctrl.wave[:1024]
+da_ctrl.seq = da_ctrl.seq[:32]
+da_ctrl.wave = np.asarray(da_ctrl.wave, dtype='<u2')
+da_ctrl.seq = np.asarray(da_ctrl.seq, dtype='<u2')
+# c = da_ctrl.seq.tobytes()
+print(len(da_ctrl.wave))
+print(len(da_ctrl.seq))
+
+_start = time.time()
+# cycle_cnt = 10000
+
+def run_non_blocking(das, batch_mode):
+    for da in das:
+        da.batch_mode = batch_mode
+    _start = time.time()
+    cycle_cnt = 10
+    t = {'1 write wave':[], '2 commit wave':[], '3 waite wave response':[],
+         '4 write cmd':[], '5 commit cmd':[], '6 waite cmd response':[],'7 send trig':[]}
+    ts = [0]*8
+    for i in range(cycle_cnt):
+        ts[0] = time.time()
+        for da in das:
+            for ch in range(4):
+                da.write_seq(ch+1,seq=da_ctrl.seq)
+                da.write_wave(ch+1,wave=da_ctrl.wave)
+                # da.write_seq_fast(ch+1,seq=da_ctrl.seq)
+                # da.write_wave_fast(ch+1,wave=da_ctrl.wave)
+        ts[1] = time.time()
+        if batch_mode:
+            for da in das:\
+                da.commit_mem()
+                # da.commit_mem_fast()
+        ts[2] = time.time()
+        if batch_mode:
+            for da in das:
+                da.wait_response()
+        ts[3] = time.time()
+        for da in das:
+            da.stop_output_wave(0)
+            da.set_trig_count_l1(60000)
+            da.set_trig_interval_l1(200e-6)
+            da.start_output_wave(1)
+            da.start_output_wave(2)
+            da.start_output_wave(3)
+            da.start_output_wave(4)
+        ts[4] = time.time()
+        if batch_mode:
+            for da in das:
+                da.commit_para()
+        ts[5] = time.time()
+        if batch_mode:
+            for da in das:
+                da.wait_response()
+        ts[6] = time.time()
+        das[0].send_int_trig()
+        ts[7] = time.time()
+        for idx, key in enumerate(t.keys()):
+            t[key].append(ts[idx+1]-ts[idx])
+    _stop = time.time()
+    print(f'board counts: {len(das)}, cycle count: {cycle_cnt}, time is: {round(_stop-_start, 5)}s')
+    return t
+
+def run(batch_mode):
+    all_t = []
+    for num in range(1,len(das)+1):
+        t = run_non_blocking(das[0:num], batch_mode)
+        all_t.append(t)
+    return all_t
+
+from numpy import *
+def process(all_t):
+    _t = {'1 write wave':[], '2 commit wave':[], '3 waite wave response':[],
+             '4 write cmd':[], '5 commit cmd':[], '6 waite cmd response':[],
+          '7 send trig':[], '8 total': [], '9 totoal without write wave':[]}
+    for t in all_t:
+        _total = 0
+        for key in t.keys():
+            avg = mean(t[key])
+            _t[key].append(avg)
+            _total += avg
+        _t['8 total'].append(_total)
+        _t[ '9 totoal without write wave'].append(_total-_t['1 write wave'][-1])
+    return _t
+
+import matplotlib.pyplot as plt
+def draw(_t, batch_mode):
+    '''ref: https://blog.csdn.net/Poul_henry/article/details/82533569'''
+    plt.figure(f'时间消耗测试batch mode-{batch_mode}')
+    # plt.title(f'time consuming test, with batch mode:{batch_mode}')
+    ax1 = plt.subplot()
+    for key in _t.keys():
+        ax1.plot(_t[key], label=key)
+    plt.xlabel(f'awg board number\n time consuming test, with batch mode:{batch_mode}')
+    plt.ylabel('time consumed(seconds)')
+    box = ax1.get_position()
+    ax1.set_position([box.x0, box.y0, box.width, box.height*0.8])
+    ax1.legend(loc='center left', bbox_to_anchor=(0.05, 1.2), ncol=2)
+
+    # plt.legend(loc='BestOutside')
+    # plt.legend()
+    plt.show()
+
+def test(batch_mode):
+    all_t = run(batch_mode)
+    _t = process(all_t)
+    draw(_t, batch_mode)
+
+test(batch_mode=True)
